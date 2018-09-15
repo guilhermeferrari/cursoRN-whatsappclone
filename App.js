7326975265b1d303f669f0577f3af43c852d7a6a
guilhermeferrari <guilhermefbravin@gmail.com>
2018-09-15 20:40:20 -0300

    instalaçao do redux e link para a cena de cadastro

--- App.js
+++ App.js
@@ -9,10 +9,14 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import Routes from './src/Routes'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 export default class App extends Component {
   render() {
     return (
-      <Routes />
+      <Provider>
+        <Routes />
+      </Provider>
     );
   }
 }
