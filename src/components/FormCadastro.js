6a7f26c2052a967f0f2477e129bbdaae643cc1cc
guilhermeferrari <guilhermefbravin@gmail.com>
2018-09-16 10:58:34 -0300

    habilitaçao de entrada do nome e ajuste do campo senha(bullet points)

--- src/components/FormCadastro.js
+++ src/components/FormCadastro.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { View, TextInput, Button } from 'react-native';
 import { connect } from 'react-redux'
+import { modificaEmail, modificaSenha, modificaNome } from '../actions/AutenticacaoActions'
 
 const formCadastro = props => (
     <View style={{ flex: 1, padding: 10 }}>
         <View style={{ flex: 4, justifyContent: 'center' }}>
-            <TextInput value={props.nome} placeholder="Nome" style={{ fontSize: 20, height: 45 }} />
-            <TextInput value={props.email} placeholder="E-mail" style={{ fontSize: 20, height: 45 }} />
-            <TextInput value={props.senha} placeholder="Senha" style={{ fontSize: 20, height: 45 }} />
+            <TextInput value={props.nome} placeholder="Nome" style={{ fontSize: 20, height: 45 }}
+                onChangeText={texto => props.modificaNome(texto) } />
+            <TextInput value={props.email} placeholder="E-mail" style={{ fontSize: 20, height: 45 }}
+                onChangeText={texto => props.modificaEmail(texto)} />
+            <TextInput secureTextEntry value={props.senha} placeholder="Senha" style={{ fontSize: 20, height: 45 }}
+                onChangeText={texto => props.modificaSenha(texto)} />
         </View>
         <View style={{ flex: 1 }}>
             <Button title="Cadastrar" color="#115E54" onPress={() => false} />
@@ -23,4 +27,4 @@ const mapStateToProps = state => (
     }
 )
 
-export default connect(mapStateToProps, null)(formCadastro)
+export default connect(mapStateToProps, { modificaEmail, modificaSenha, modificaNome })(formCadastro)
