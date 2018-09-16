09acb3228261ec4e89d46a865800766926aac037
guilhermeferrari <guilhermefbravin@gmail.com>
2018-09-16 14:55:20 -0300

    configuracoes padroes do firebas

--- App.js
+++ App.js
@@ -12,8 +12,23 @@ import Routes from './src/Routes'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducers from './src/reducers/index'
+import firebase from 'firebase'
+import {key as apiKey} from './firebaseconfig.json'
 
 export default class App extends Component {
+
+	componentWillMount(){
+		let config = {
+			apiKey: apiKey,
+			authDomain: "whatsapp-clone-e01f1.firebaseapp.com",
+			databaseURL: "https://whatsapp-clone-e01f1.firebaseio.com",
+			projectId: "whatsapp-clone-e01f1",
+			storageBucket: "whatsapp-clone-e01f1.appspot.com",
+			messagingSenderId: "653398324919"
+		};
+		firebase.initializeApp(config);
+	}
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
