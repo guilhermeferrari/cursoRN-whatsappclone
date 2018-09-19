8e2c17a93fc81f0a85f98aa91f535e224164540b
guilhermeferrari <guilhermefbravin@gmail.com>
2018-09-19 15:37:25 -0300

    OK ate aula 282. Recebendo as mensagens em Conversas

--- src/components/Conversa.js
+++ src/components/Conversa.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, ListView, StyleSheet, TextInput, Image, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux'
-import { modificaMensagem, enviarMensagem } from '../actions/AppActions';
+import { modificaMensagem, enviarMensagem, conversaUsuarioFetch } from '../actions/AppActions';
 import _ from 'lodash'
 import { COLOR_HTML_P, COLOR_HTML_S, borderRADIUS_PADRAO, styles } from './FormLogin';
 
@@ -9,15 +9,46 @@ const btnEnviar = require('../imgs/enviar.png')
 
 class Conversa extends Component {
 
-    _enviarMensagem(){
-        const {mensagem, contatoNome, contatoEmail} = this.props
+    componentWillMount() {
+        this.props.conversaUsuarioFetch(this.props.contatoEmail)
+        this.criaFonteDeDados(this.props.conversa)
+    }
+
+    componentWillReceiveProps(nextProps){
+        this.criaFonteDeDados(nextProps.conversa)
+    }
+
+    _enviarMensagem() {
+        const { mensagem, contatoNome, contatoEmail } = this.props
         this.props.enviarMensagem(mensagem, contatoNome, contatoEmail)
+    }
+
+    criaFonteDeDados(conversa) {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        })
+        this.dataSource = ds.cloneWithRows(conversa)
+    }
+
+    renderRow(texto){
+        return(
+            <View>
+                <Text>{texto.mensagem}</Text>
+                <Text>{texto.tipo}</Text>
+            </View>
+        )
     }
 
     render() {
         return (
             <View style={stylesConversa.viewPrincipal}>
-                <View style={stylesConversa.viewChat}></View>
+                <View style={stylesConversa.viewChat}>
+                    <ListView
+                        enableEmptySections
+                        dataSource={this.dataSource}
+                        renderRow={this.renderRow}
+                    />
+                </View>
                 <View style={stylesConversa.viewEntrada}>
                     <TextInput
                         style={stylesConversa.entrada}
@@ -59,8 +90,14 @@ const stylesConversa = StyleSheet.create({
     }
 });
 
-const mapStateToProps = state => ({
-    mensagem: state.AppReducer.mensagem
-})
+const mapStateToProps = state => {
+    const conversa = _.map(state.ListaConversaReducer, (val, uid) => {
+        return { ...val, uid }
+    })
+    return ({
+        conversa,
+        mensagem: state.AppReducer.mensagem
+    })
+}
 
-export default connect(mapStateToProps, { modificaMensagem, enviarMensagem })(Conversa)
+export default connect(mapStateToProps, { modificaMensagem, enviarMensagem, conversaUsuarioFetch })(Conversa)
